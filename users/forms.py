580ef6c0aa7225e0712d18ab9796d9e43392e9a8
djangorepos <ex.timwidow@gmail.com>
2022-04-29 22:30:50 +0300

    input required

--- users/forms.py
+++ users/forms.py
@@ -5,11 +5,12 @@ from .models import User
 
 class UserForm(UserCreationForm):
 
-    """ def __init__(self, *args, **kwargs):
-         super(UserForm, self).__init__(*args, **kwargs)
-         for field_name, field in self.fields.items():
-             field.widget.attrs['class'] = 'form-control """
+    def __init__(self, *args, **kwargs):
+        super(UserForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
 
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email', 'type']
+        required_fields = ['username', 'first_name', 'last_name', 'email', 'type']
